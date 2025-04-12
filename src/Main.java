3243e62d894bfd5206801878d3879aec7c749bef
Yvette IRADUKUNDA <iradyvette98@gmail.com>
2025-04-12 14:52:03 +0200

    Adding changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         TaskManager manager = new TaskManager();
-
+       //manager is an object of a class TaskManager
         try {
+            System.out.println("Added tasks:");
             manager.addTask("Go to Market");
             manager.addTask("Learning Python and java");
             manager.addTask("Do some exxercises");
@@ -15,15 +16,15 @@ public class Main {
             manager.sortTasks();
             System.out.println("Sorted Tasks: " + manager.getAllTasks());
 
-            try {
+            try { //try any task initialized before and see if you find the task
                 System.out.println("Found 'Learning Python and java' at index: " + manager.searchTask("Learning Python and java"));
-                manager.searchTask("Study python");
-                // Will throw TaskNotFoundException,
+                manager.searchTask("Study python"); //try any
+                // Will throw TaskNotFoundException because 'study python' is not one of the tasks listed,
             } catch (TaskNotFoundException e) {
                 System.err.println("Error: " + e.getMessage());
             }
 
-            try {manager.removeTask("Go to Market");
+            try {manager.removeTask("Go to Market"); // after removing the task, all tasks remains will be displayed
                 System.out.println("Tasks after removal: " + manager.getAllTasks());
                 manager.removeTask("Clean room"); // Will throw TaskNotFoundException
             } catch (TaskNotFoundException e) {System.err.println("Error: " + e.getMessage());
@@ -31,31 +32,36 @@ public class Main {
             System.out.println("\nLongest Task: " + manager.findLongestTask());
 
             try {System.out.println("Task at index 0: " + manager.getTaskByIndex(0));
-                manager.getTaskByIndex(5);
+                manager.getTaskByIndex(3);
                 // manager.getTaskByIndex(5); Will throw IndexOutOfBoundsException
                 // because it is invalid index, try to be one of the tasks above and check
             } catch (IndexOutOfBoundsException e) {System.err.println("Error: " + e.getMessage());
             }
 
             // Testing invalid inputs
-            try {manager.addTask(null);
-            } catch (IllegalArgumentException e) {System.err.println("Error: " + e.getMessage());
+            try {
+                manager.addTask(null);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Error: " + e.getMessage());
             }
             try {
-                manager.searchTask("");
-            } catch (IllegalArgumentException e) {System.err.println("Error:" + e.getMessage());
+                manager.searchTask("Plan weekend trip");
+            } catch (IllegalArgumentException e) {
+                System.err.println("Error:" + e.getMessage());
             } catch (TaskNotFoundException e) {
                 throw new RuntimeException(e);
             }
             try {
                 manager.removeTask("Go to Market ");
-            } catch (IllegalArgumentException e) {System.err.println("Error:" + e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.err.println("Error:" + e.getMessage());
             } catch (TaskNotFoundException e) {
-                throw new RuntimeException(e);
+                
             }
             try {
                 manager.getTaskByIndex(-1);
-            } catch (IndexOutOfBoundsException e) {System.err.println("Error: " + e.getMessage());
+            } catch (IndexOutOfBoundsException e) {
+                System.err.println("Error: " + e.getMessage());
             }
 
         } finally {System.out.println("All operations complete.");
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-//thi is a custom exception,
+//TaskNotFoundException is a custom exception,
 // it indicates when a search/removal operation fails because the task doesn't exist.
 class TaskNotFoundException extends Exception {
     public TaskNotFoundException(String message) {
@@ -11,7 +11,7 @@ class TaskNotFoundException extends Exception {
 // our class is called TaskManager
 class TaskManager {
     private List<String> tasks = new ArrayList<>();
-
+    //this is a method add tasks
     public void addTask(String task) {
         //.trim().isEmpty() is a method handles cases where the user enters only whitespace or a null string as a task.
 
@@ -19,30 +19,29 @@ class TaskManager {
             throw new IllegalArgumentException("Task cannot be empty."); // put a task to check
         }
         tasks.add(task.trim()); //.trim() is a method used to remove whitespace
-        System.out.println("Added: " + task.trim());
+        System.out.println(task.trim());
     }
 
     public List<String> getAllTasks() {
 
         return new ArrayList<>(tasks);
     }
-
+       //this is a method to sort to tasks
     public void sortTasks() {
         Collections.sort(tasks);
-        System.out.println("Tasks are already sorted.");//
     }
-
+          //this is a method to search for a specific tasks
     public int searchTask(String task) throws TaskNotFoundException {
         if (task == null || task.trim().isEmpty()) {
             throw new IllegalArgumentException("This cannot be empty.");
         }
-        int index = tasks.indexOf(task.trim());
+        int index = tasks.indexOf(task.trim()); // if the task is found, it will also display its index
         if (index == -1) {
             throw new TaskNotFoundException("Task '" + task.trim() + "' not found.");
         }
         return index;
     }
-
+          //this is the method to remove task from others
     public void removeTask(String task) throws TaskNotFoundException {
         if (task == null || task.trim().isEmpty()) {
             throw new IllegalArgumentException("removal cannot be empty.");
@@ -52,7 +51,7 @@ class TaskManager {
         }
          System.out.println("Removed: " + task.trim());
     }
-
+       //this is method finds the longest task but if the task is empty, an error will be displayed
     public String findLongestTask() {
         if (tasks.isEmpty()) return null;
         String longest = "";
@@ -63,7 +62,8 @@ class TaskManager {
         }
         return longest;
     }
-
+    // this is a method to get element by index but if index is less to zero or
+    // index is greater or equal to the size of tasks, error will be displayed.
     public String getTaskByIndex(int index) {
         if (index < 0 || index >= tasks.size()) {
             throw new IndexOutOfBoundsException("Invalid index: " + index);
